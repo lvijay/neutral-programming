23f298b0ec04d15ca847ae4418e3b36c203a4b9a
Vijay Lakshminarayanan <laksvij@hawk.iit.edu>
2020-12-19 18:49:53 +0530

    detect unique char set, "support" ruby also

--- generate.py
+++ generate.py
@@ -1,41 +1,59 @@
 #!/usr/bin/env python3
 
-CATEGORIES = {'␠': set(' '),
-              '␤': set('\n'),
-              'α': set('s p l i n t e r f d'.split()),
-              '#': set('#'),
-              '(': set('('),
-              ')': set(')'),
-              '"': set('"'),
-              ':': set(':')}
-
-CAT_PREFIX = {'␠': 'space',
-              '␤': 'nline',
-              'α': 'alpha',
-              '#': 'pound',
-              '(': 'preno',
-              ')': 'prenc',
-              '"': 'dquot',
-              ':': 'colon'}
+import os.path
 
 def corpus(filename):
     with open(filename) as fn:
         return fn.read()
 
-def generate(filename, odir):
+def get_category(char):
+    if char.isalpha(): return 'α'
+    if char.isdigit(): return '𝓃'
+    if char in ' ': return '␠'
+    if char in '\n': return '␤'
+    return char              # other characters are their own category
+
+def get_catprefix(category):
+    return {
+        'α': 'alphabet',
+        '𝓃': 'digit',
+        '␠': 'space',
+        '␤': 'newline',
+        '(': 'paren-open',
+        ')': 'paren-close',
+        '_': 'underscore',
+        '"': 'dquote',
+        "'": 'squote',
+        '#': 'hash',
+        ':': 'colon'
+    }[category]
+
+def generate(filename, odir, vals, exec_content_fn):
     data = corpus(filename)
-    width = len(str(len(data) * len(CATEGORIES)))
-    for k, v in CATEGORIES.items():
-        fileprefix = CAT_PREFIX[k]
+    categories = dict(map(lambda c: (get_category(c), c), data))
+    ext = filename[filename.index('.')+1:] # extension
+    for k, v in categories.items():
+        fileprefix = get_catprefix(k)
         candidate = next(iter(v))
-        for i in range(len(data)):
+        for i in range(1, 1+len(data)):
             prefix, suffix = data[:i], data[i:]
-            filename = fileprefix + f'{i:#0{width}d}.py'
-            with open(odir + '/' + filename, 'w') as out:
-                out.write(prefix + candidate + suffix)
+            filename = lambda c, v: f'{fileprefix}_{c}_{v}.{ext}'
+            content = prefix + candidate + suffix
+            for val in vals:
+                fname = filename(i, val)
+                exec_content = exec_content_fn(val)
+                with open(os.path.join(odir, fname), 'w') as out:
+                    print(prefix + candidate + suffix, file=out)
+                    print(exec_content, file=out)
 
 if __name__ == '__main__':
     import sys
-    generate(sys.argv[1], sys.argv[2])
+    i = 1
+    inputfile, i = sys.argv[i], i+1
+    outputdir, i = (sys.argv[i], i+1) if len(sys.argv) >= 2 else ('.', i)
+    ispy = inputfile.endswith('.py')
+    vals = '01' if ispy else ('nil', '1')
+    efn = (lambda x: f'\ndin({x})') if ispy else (lambda x: f'\ndeed {x}')
+    generate(inputfile, outputdir, vals, efn)
 
 ## generate.py ends here
